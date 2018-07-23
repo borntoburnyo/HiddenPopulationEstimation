371c8d0c09335c6f30367c1a63283151fdb49558
Demo <demodemowhitebig@gmail.com>
2018-07-23 16:28:05 -0400

    Update labeledDataGeneration.R

--- dataGeneration/labeledDataGeneration.R
+++ dataGeneration/labeledDataGeneration.R
@@ -3,6 +3,10 @@
 #two clusters have different size and different probabilities to be sampled 
 
 sim_LabeledData <- function(N, n, cluster.props, freq) {
+    #N: total population size
+    #n: population to be captured
+    #cluster.props: list of fractions that sum to 1, describes proportion of people in each cluster
+    #freq: list of fractions sum to 1, describes probabilities of people being in forest for each cluster
     n_clusters <- length(cluster.props) # number of clusters
     N_cluster <- round(N * cluster.props) # number of people in each cluster (population)
     N_cluster[1] <- N_cluster[1] + (N - sum(N_cluster)) # make sure that all people sum to N
